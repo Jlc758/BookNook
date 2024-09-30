502a2b2b0b65523a49d5200261f365720037bda8
Jlc758 <janelle.chafe@gmail.com>
2024-09-30 20:28:30 -0230

    Troubleshooting deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
-import "../css/index.css";
-import Layout from "../context/Layout";
-import LandingPage from "../pages/LandingPage";
-import SearchOptions from "../pages/SearchOptionsPage";
-import Shelves from "../pages/Shelves";
-import Theme from "../css/theme";
-import SearchProvider from "../context/SearchContext";
-import ShelfProvider from "../context/ShelfContext";
-import AvatarFAB from "./AvatarFAB";
+import "./css/index.css";
+import Layout from "./context/Layout";
+import LandingPage from "./pages/LandingPage";
+import SearchOptions from "./pages/SearchOptionsPage";
+import Shelves from "./pages/Shelves";
+import Theme from "./css/theme";
+import SearchProvider from "./context/SearchContext";
+import ShelfProvider from "./context/ShelfContext";
+import AvatarFAB from "./components/AvatarFAB";
 import { useState, useEffect } from "react";
 
 const App = () => {
