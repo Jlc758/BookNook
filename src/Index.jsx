66b0a18e5af58a59d8eab9fb3382a937351692b6
Jlc758 <janelle.chafe@gmail.com>
2024-09-30 20:42:50 -0230

    Troubleshooting cont'd

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Navigation from "./Navigation.jsx";
+import Navigation from "./components/Navigation.jsx";
 
 const Index = () => {
   return (
